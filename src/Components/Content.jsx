f2a2520bb68f63ab2be4f9ab0f8274b37c38eb39
vallarasuk <vallarasuk143@gmail.com>
2023-05-23 08:26:23 +0530

    content aligned commit

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,43 +1,21 @@
 import React from "react";
 import "./style.css";
+
 const Content = () => {
   return (
     <div
-      style={{
-        backgroundImage: "url('https://picsum.photos/seed/picsum/200/300')",
-        backgroundSize: "100% auto",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        width: "100%",
-        height: "100vh",
-        borderRadius: "10%",
-        // display: "flex",
-        // alignItems: "center",
-        // justifyContent: "center",
-      }}
+      className="main-content d-flex align-items-center justify-content-center"
     >
-      {/* <div className="container text-center text-white">
-    <h1 className="display-1 fw-bold heading d-flex justify-content-center align-items-center w-75 bg-danger">
-      Reliable kidcare on demand
-    </h1>
-    <p>
-      Otter matches parents who need care with trusted sitters in their community, on-demand.
-    </p>
-    <button className="btn btn-primary rounded-6">Sign Up</button>
-  </div> */}
-
-      <div className="row text-center text-white">
-        <div className="col-2 col-sm-2"><h1></h1></div>
-        <div className="col-8 col-sm-12">
-          <h1 className="display-1 fw-bold heading ">
-            Reliable kidcare on demand
-          </h1>
-          <p>
-      Otter matches parents who need care with trusted sitters in their community, on-demand.
-    <button className="btn btn-primary rounded-6">Sign Up</button>
-    </p>
+      <div className="container text-center text-white">
+        <div className="row">
+          <div className="col-12 col-lg-8 offset-lg-2">
+            <h1 className="display-4 fw-bold heading main-content-h1">Reliable kidcare on demand</h1>
+            <p className="main-content-p">
+              Otter matches parents who need care with trusted sitters in their community, on-demand.
+            </p>
+            <button className=" button-style">Sign Up</button>
+          </div>
         </div>
-        <div className="col-2 col-sm-2 "></div>
       </div>
     </div>
   );
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,13 +10,13 @@ const Header = () => {
                 <a href="https://withotter.com/trust-safety" className="text-decoration-none">Trust and Safety</a>
                 <a href="https://withotter.com/trust-safety" className="text-decoration-none">Blog</a>
             </div>
-            <div className="col-2 mt-4 text-center ms-5 ">
-              <a href="https://withotter.com/"><img src="https://assets.website-files.com/6424ac4b25764708c6aa49f7/642e39266f382a63e8e7e853_Otter%20Icon.png" alt="Otto" width={"100%"}  height={"70%"}/></a>
+            <div className="col-2 text-center  my-5">
+              <a href="https://withotter.com/" className='logo text-decoration-none p-3 ' >otter</a>
             </div>
             <div className="col-4 py-5 justify-content-end d-flex my-4">
               <a href="https://app.withotter.com/log-in?device-id=zV2leBbFxJ7xn0FYHcVKtO" className="fw-bold mx-2 text-decoration-none btn ">Log in</a>
               
-              <a href="https://app.withotter.com/log-in?device-id=zV2leBbFxJ7xn0FYHcVKtO" className="fw-bold mx-2 text-decoration-none btn btn-outline-secondary rounded-5 mx-2">Sign up</a>
+              <a href="https://app.withotter.com/log-in?device-id=zV2leBbFxJ7xn0FYHcVKtO" className="button-style text-decoration-none">Sign up</a>
             </div>
         </div>
       
