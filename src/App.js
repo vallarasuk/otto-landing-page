b0d0be4341de14e3b2926c7976cfdcf95a74bb1b
Vallarasu Kanthasamy <vallarasu.kanthasamy@thidiff.com>
2023-05-27 18:08:50 +0530

    Fourth section added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Header from './Components/Header'
 import Content from './Components/Content'
 import SecondSlideContent from './Components/ContentSection/SecondSlideContent'
 import ThirdSlideContent from './Components/ContentSection/ThirdSlideContent'
+import FourthSectionContent from './Components/ContentSection/FourthSectionContent'
 
 const App = () => {
   return (
     <>
      <div className="container">
-    <Header/><Content/><SecondSlideContent/><ThirdSlideContent/>
+    <Header/><Content/><SecondSlideContent/><ThirdSlideContent/><FourthSectionContent/>
       </div> 
     </>
   )
---
+++ src/Components/ContentSection/FourthSectionContent.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const FourthSectionContent = () => {
+  return (
+    <div className="second-slide-main section-four my-5 rounded-5 shadow">
+      <div className="py-5 text-center">
+        <div className="row d-flex justify-content-between mx-5 align-item-center">
+          <div className="col">AS FEATURED IN</div>
+          <div className="col">
+            <img
+              src="https://assets.website-files.com/6424ac4b25764708c6aa49f7/642b1c26f78691df83e63005_forbes.svg"
+              alt="Fobes"
+            />
+          </div>
+          <div className="col">
+            <img
+              src="https://assets.website-files.com/6424ac4b25764708c6aa49f7/642b1c916900c5313c377f14_parents.svg"
+              alt="Parents"
+            />
+          </div>
+          <div className="col">
+            <img
+              src="https://assets.website-files.com/6424ac4b25764708c6aa49f7/642b1c266900c5c1e13779cb_NYT.svg"
+              alt="new york towns"
+            />
+          </div>
+          <div className="col">
+            <img
+              src="https://assets.website-files.com/6424ac4b25764708c6aa49f7/642b1c267c52ff922aad871b_mom.svg"
+              alt="mom.com"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FourthSectionContent;
